075bd154557c062487f10c791679ca60500f5950
jared <767882503@qq.com>
2019-03-26 16:38:35 +0800

    在线编译

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 ; (function () {
     "use strict"
+    function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+
+    function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+    function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+    function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+    function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+    function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+    function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+    function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+    function _wrapNativeSuper(Class) { var _cache = typeof Map === "function" ? new Map() : undefined; _wrapNativeSuper = function _wrapNativeSuper(Class) { if (Class === null || !_isNativeFunction(Class)) return Class; if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() { return _construct(Class, arguments, _getPrototypeOf(this).constructor); } Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _setPrototypeOf(Wrapper, Class); }; return _wrapNativeSuper(Class); }
+
+    function isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () { })); return true; } catch (e) { return false; } }
+
+    function _construct(Parent, args, Class) { if (isNativeReflectConstruct()) { _construct = Reflect.construct; } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
+
+    function _isNativeFunction(fn) { return Function.toString.call(fn).indexOf("[native code]") !== -1; }
+
+    function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+    function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+    ;
+
     var _global;
     var style = document.createElement('style');
     style.type = 'text/css';
@@ -219,43 +249,99 @@
 
     // window.customElements.define('date-year-picker', YearPicker);
 
-    var YearPicker = Object.create(HTMLElement.prototype, {
-        /* 元素生命周期的事件 */
-        // 实例化时触发
-        createdCallback: {
-            value: function () {
-                // console.log('invoked createCallback!');
-            },
-        },
-        // 元素添加到DOM树时触发
-        attachedCallback: {
-            value: function () {
-                // console.log('invoked attachedCallback!');
-            },
-        },
-        // 元素DOM树上移除时触发
-        detachedCallback: {
-            value: function () {
-                // console.log('invoked detachedCallback!');
-            },
-        },
-        // 元素的attribute发生变化时触发
-        attributeChangedCallback: {
-            value: function (attrName, oldVal, newVal) {
-                // console.log('attributeChangedCallback-change' + attrName + 'from' + oldVal + 'to' + newVal);
-                customTag('date-year-picker', myElementHandler);
-            },
-        },
-        /* 定义元素的公有方法和属性 */
-        // 重写  属性
-        value: {
-            get: function () { return this.getAttribute('value'); },
-            set: function (val) { this.setAttribute('value', val); },
-        },
+    // var YearPicker = Object.create(HTMLElement.prototype, {
+    //     /* 元素生命周期的事件 */
+    //     // 实例化时触发
+    //     createdCallback: {
+    //         value: function () {
+    //             // console.log('invoked createCallback!');
+    //         },
+    //     },
+    //     // 元素添加到DOM树时触发
+    //     attachedCallback: {
+    //         value: function () {
+    //             // console.log('invoked attachedCallback!');
+    //         },
+    //     },
+    //     // 元素DOM树上移除时触发
+    //     detachedCallback: {
+    //         value: function () {
+    //             // console.log('invoked detachedCallback!');
+    //         },
+    //     },
+    //     // 元素的attribute发生变化时触发
+    //     attributeChangedCallback: {
+    //         value: function (attrName, oldVal, newVal) {
+    //             // console.log('attributeChangedCallback-change' + attrName + 'from' + oldVal + 'to' + newVal);
+    //             customTag('date-year-picker', myElementHandler);
+    //         },
+    //     },
+    //     /* 定义元素的公有方法和属性 */
+    //     // 重写  属性
+    //     value: {
+    //         get: function () { return this.getAttribute('value'); },
+    //         set: function (val) { this.setAttribute('value', val); },
+    //     },
 
-    });
+    // });
 
-    document.registerElement('date-year-picker', { prototype: YearPicker });
+    // window.customElements.define('date-year-picker', YearPicker);
+    // document.registerElement('date-year-picker', { prototype: YearPicker });
+
+    var YearPicker =
+        /*#__PURE__*/
+        function (_HTMLElement) {
+            _inherits(YearPicker, _HTMLElement);
+
+            function YearPicker() {
+                _classCallCheck(this, YearPicker);
+
+                return _possibleConstructorReturn(this, _getPrototypeOf(YearPicker).call(this));
+            }
+
+            _createClass(YearPicker, [{
+                key: "createdCallback",
+
+                /* 元素生命周期的事件 */
+                // 实例化时触发
+                value: function createdCallback() { } // console.log('invoked createCallback!');
+                // 元素添加到DOM树时触发
+
+            }, {
+                key: "attachedCallback",
+                value: function attachedCallback() { } // console.log('invoked attachedCallback!');
+                // 元素DOM树上移除时触发
+
+            }, {
+                key: "detachedCallback",
+                value: function detachedCallback() { } // console.log('invoked detachedCallback!');
+                // 元素的attribute发生变化时触发
+
+            }, {
+                key: "attributeChangedCallback",
+                value: function attributeChangedCallback(attrName, oldVal, newVal) {
+                    // console.log('attributeChangedCallback-change' + attrName + 'from' + oldVal + 'to' + newVal);
+                    customTag('date-year-picker', myElementHandler);
+                }
+            }, {
+                key: "value",
+                get: function get() {
+                    return this.getAttribute('value');
+                },
+                set: function set(val) {
+                    this.setAttribute('value', val);
+                }
+            }], [{
+                key: "observedAttributes",
+                get: function get() {
+                    return ['value'];
+                }
+            }]);
+
+            return YearPicker;
+        }(_wrapNativeSuper(HTMLElement));
+
+    window.customElements.define('date-year-picker', YearPicker);
 
     function customTag(tagName, fn) {
         Array
@@ -399,7 +485,7 @@
         element.appendChild(pickerIcon);
         if (value) {
             var pickerClear = createIcon('pickerClear', clearPath);
-            pickerClear.addEventListener('click', function (e){
+            pickerClear.addEventListener('click', function (e) {
                 element.value = '';
                 onChange('', element);
             });
